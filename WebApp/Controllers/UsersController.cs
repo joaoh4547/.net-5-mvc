9a7c3d071ebe3c54354349768ce5360231d6785b
João Henrique <joaohenriquetdj@gmail.com>
2021-11-07 16:11:50 -0300

    add initi users controler and index view

--- WebApp/Controllers/UsersController.cs
+++ WebApp/Controllers/UsersController.cs
@@ -1,18 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using WebApp.Models.ViewModels;
 
 namespace WebApp.Controllers
 {
-    public class HomeController : Controller
+    public class UsersController : Controller
     {
-      
-
         public IActionResult Index()
         {
             return View();
