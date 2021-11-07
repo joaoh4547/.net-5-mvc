5beb60257f71ff543b2c74f9cc82e830170df152
João Henrique <joaohenriquetdj@gmail.com>
2021-11-07 17:46:45 -0300

    add Setup for Entity Framewok Core With PostgreSQL

---
+++ WebApp/Data/WebAppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Data
+{
+    public class WebAppDbContext : DbContext
+    {
+        public WebAppDbContext(DbContextOptions<WebAppDbContext> options): base(options)
+        {
+
+        }
+    }
+}
--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -4,10 +4,12 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApp.Data;
 
 namespace WebApp
 {
@@ -24,6 +26,7 @@ namespace WebApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<WebAppDbContext>(opt => opt.UseNpgsql(Configuration.GetConnectionString("WebAppConnectionString"),builder => builder.MigrationsAssembly("WebApp")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
