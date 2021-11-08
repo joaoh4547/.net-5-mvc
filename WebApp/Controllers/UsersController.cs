a4da97e1e435d16303e9f019c0da445c17115a8c
João Henrique <joaohenriquetdj@gmail.com>
2021-11-07 21:36:12 -0300

    add init create user form layout

--- WebApp/Controllers/UsersController.cs
+++ WebApp/Controllers/UsersController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebApp.Models;
+using WebApp.Models.ViewModels;
 using WebApp.Services;
 
 namespace WebApp.Controllers
@@ -23,5 +24,13 @@ namespace WebApp.Controllers
             var users = await _userService.FindAllAsync();
             return View(users);
         }
+
+        public IActionResult Create()
+        {
+            var viewModel = new UserViewModel();
+            return View(viewModel);
+        }
+        
+        
     }
 }
